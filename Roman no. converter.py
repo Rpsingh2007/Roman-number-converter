0abe0ffce6c38f10629e4e018bbe142f9a7e20c5
Rpsingh2007 <rajputrpsingh91@gmail.com>
2026-01-13 15:57:54 +0530

    Updated--version

--- Roman no. converter.py
+++ Roman no. converter.py
@@ -1,3 +1,4 @@
+# Function to convert integer to Roman numeral
 def int_to_roman(num):
     roman_dict = {
         1000: "M", 900: "CM",
@@ -15,17 +16,7 @@ def int_to_roman(num):
             num -= value
     return roman
 
-
-#  Take user input
-number = int(input("Enter a number (1 to 1000): "))
-
-if 1 <= number <= 1000:
-    print("Roman numeral:", int_to_roman(number))
-else:
-    print("Please enter a number between 1 and 1000.")
-
-
-
+# Function to convert Roman numeral to integer
 def roman_to_integer(roman):
     roman_dict = {
     'I': 1,
@@ -51,15 +42,47 @@ def roman_to_integer(roman):
     
     return total
 
+# Main program loop
+print(40*"=")
+print ("Welcome to the Roman Numeral Converter!")
+print(40*"=")
+while True:
+    operation=input("Enter '1' to convert Integer to Roman \n"\
+                    "Enter'2' to convert Roman to Integer\n"\
+                    "Enter 'q' to quit\n"\
+                    "Choose operation: ")
+    print(40*"=")
+    if operation.lower()=="q":
+        print("     Exiting the program.")
+        print()
+        print("--------Visit Again!--------")
+        print(40*"=")
+        break
+    if operation=="1":
+        #  Take user input
+        number = int(input("Enter a number (1 to 1000): "))
+        
+        if 1 <= number <= 1000:
+            print("Roman numeral:", int_to_roman(number))
+            print(40*"=")
+        else:
+            print("Please enter a number between 1 and 1000.")
+            print(40*"=")
 
-# ---- User Input ----
-roman_num = input("Enter a Roman number (I to M): ")
-
-result = roman_to_integer(roman_num)
-
-if 1 <= result <= 1000:
-    print("Integer value:", result)
-else:
-    print("Invalid Roman numeral or out of range (1–1000)")
-
-
+    elif operation=="2":
+        # ---- User Input ----
+        roman_num = input("Enter a Roman number (I to M): ")
+        
+        result = roman_to_integer(roman_num)
+        
+        if 1 <= result <= 1000:
+            print("Integer value:", result)
+            print(40*"=")
+        else:
+            print("Invalid Roman numeral or out of range (1–1000)")
+            print(40*"=")
+    else:
+        print("Invalid operation. Please try again.")
+        continue  
+        
+        
